7faaa6a52ca62fd77277ec0d520300b7c4762599
Brad Nguyen <brad.nguyen@mrjaffesclass.com>
2023-09-18 11:31:28 -0700

    part one done

--- IfStatements.java
+++ IfStatements.java
@@ -1,7 +1,24 @@
-public class IfStatements 
+public class IfStatements
 {
-    public static void main(String[] args) 
-        {
-            
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        if (a == 5 ) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
         }
+        System.out.println("Hello World!");
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        IfStatements hw = new IfStatements();
+        hw.run();
+    }
 }
